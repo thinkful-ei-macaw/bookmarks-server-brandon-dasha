84adab30c422b876179e33e8167776d6428dfd9e
dasha akhten <dasha.akhten@gmail.com>
2020-03-27 09:46:40 -0700

    first commit

---
+++ src/store.js
@@ -0,0 +1,25 @@
+const uuid = require('uuid/v4')
+
+const bookmarks = [
+    {
+        id: uuid(),
+        title: 'Thinkful',
+        url: 'https://www.thinkful.com',
+description: 'coding bootcamp',
+rating:5
+    },
+    {
+        id: uuid(),
+        title: 'instagram',
+        url: 'https://www.instagram.com',
+description: 'ouroboros of desperation',
+rating:1
+    },
+    {
+        id: uuid(),
+        title: 'Google',
+        url: 'https://www.google.com',
+description: 'find any website!',
+rating:5
+    },
+]
